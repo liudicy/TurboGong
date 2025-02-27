ac99ca29a42fd128992954e189dd16950afc5add
liudi <liudicy@vip.qq.com>
2025-02-27 16:45:08 +0800

    精简文件目录

--- src/script_generator_ui.py
+++
@@ -1,6 +0,0 @@
-import tkinter as tk
-from tkinter import filedialog, messagebox
-from src.template_validator import TemplateValidator
-from src.config_generator import ConfigGenerator
-
-# ... 其余代码保持不变 ... 
